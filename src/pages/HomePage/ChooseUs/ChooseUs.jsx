7af5a0d73b9d78a0094fa4d39277b4bab472f7e6
fabrazzak <freelancerabdurrazzak47@gmail.com>
2024-12-07 02:42:48 +0600

    reveal use done

--- src/pages/HomePage/ChooseUs/ChooseUs.jsx
+++ src/pages/HomePage/ChooseUs/ChooseUs.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
 import img from "../../../assets/choose-us.jpg"
+import { Slide } from "react-awesome-reveal";
+import { Fade } from "react-awesome-reveal";
+
+
+
 
 const ChooseUs = () => {
     return (
+       
         <div className='container mx-auto md:w-10/12 py-10'>
+            <Fade direction="left" delay={500} duration={2000} cascade damping={0.3}>
+                
+           
+               
+           
 
             <h2 className='section-title text-center md:text-4xl text-2xl font-bold pb-8'>Why Choose Us </h2>
 
@@ -17,6 +28,7 @@ const ChooseUs = () => {
                         <h2 className='section-title text-center md:text-2xl text-xl font-bold pb-6'>Why Shop with EquiSports?</h2>
                        
                         <div className="card-bod p-10">
+                           
                              
                             <ul className='flex flex-col gap-5 md:text-2xl'>
                                 <li className='font-bold list-disc '>High-Quality Products</li>
@@ -24,7 +36,11 @@ const ChooseUs = () => {
                                 <li className='font-bold list-disc '>Fast Delivery Worldwide</li>
                                 <li className='font-bold list-disc '>Customer-Centric Support</li>
                                 <li className='font-bold list-disc '>Secure Payments</li>
+                              
+                                   
+                               
                             </ul>                        
+                            
                             
                         </div>
                     </div>
@@ -33,8 +49,11 @@ const ChooseUs = () => {
                 </div>
               
             </div>
+            </Fade>
+           
 
         </div>
+        
     );
 };
 
