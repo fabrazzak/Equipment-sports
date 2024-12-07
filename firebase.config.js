084435bc097d8c1509c4a320f4781fc39796ff4d
fabrazzak <freelancerabdurrazzak47@gmail.com>
2024-12-07 23:46:13 +0600

    update code

--- firebase.config.js
+++ firebase.config.js
@@ -3,12 +3,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyA-IzjdLraTrNBhM6OB1XreW0mNAQ8R36M",
-    authDomain: "equi-sports-6a027.firebaseapp.com",
-    projectId: "equi-sports-6a027",
-    storageBucket: "equi-sports-6a027.firebasestorage.app",
-    messagingSenderId: "671349157711",
-    appId: "1:671349157711:web:fe62f61b854ba74fa5df3a",
+    apiKey:import.meta.env.VITE_API_KEY,
+    authDomain:import.meta.env.VITE_AUTH_DOMAIN,
+    projectId:import.meta.env.VITE_PROJECT_ID,
+    storageBucket:import.meta.env.VITE_STORAGE_BUCKET,
+    messagingSenderId:import.meta.env.VITE_MESSAGING_SENDER_ID,
+    appId:import.meta.env.VITE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
